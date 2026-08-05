9d6c1fb963634f9d520ab67970ed46168e81cb80
thuctran112 <168316224+thuctran112@users.noreply.github.com>
2026-08-05 13:18:22 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thuctran112.github.io',
   integrations: [mdx()],
-  output: 'static'
+  output: 'hybrid',
+  adapter: node({
+    mode: 'standalone'
+  })
 });
